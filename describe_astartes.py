f103664b06a30d7e204e58a8643f84c28c44003c
brad-wilkes <rbwilkes@yahoo.com>
2025-04-12 16:50:34 -0500

    added new attribute to legions dict

--- describe_astartes.py
+++ describe_astartes.py
@@ -3,92 +3,110 @@ legions = {
     'sons of horus' : {
         'primarch' : 'horus',
         'traitor' : True,
-        'number' : 16
+        'number' : 16,
+        'battlecry' : 'For the Warmaster'
     },
     'ultramarines' : {
         'primarch' : 'guilliman',
         'traitor' : False,
-        'number' : 13
+        'number' : 13,
+        'battlecry' : 'For Macragge'
     },
     'world eaters' : {
         'primarch' : 'angron',
         'traitor' : True,
-        'number' : 12
+        'number' : 12,
+        'battlecry' : 'For Khorne'
     },
     'iron warriors' : {
         'primarch' : 'perturabo',
         'traitor' : True,
-        'number' : 10
+        'number' : 10,
+        'battlecry' : 'For the Iron Cage'
     },
     'salamanders' : {
         'primarch' : 'vulkan',
         'traitor' : False,
-        'number' : 18
+        'number' : 18,
+        'battlecry' : 'For Nocturne'
     },
     'raven guard' : {
         'primarch' : 'corax',
         'traitor' : False,
-        'number' : 19
+        'number' : 19,
+        'battlecry' : 'For Deliverance'
     },
     'imperial fists' : {
         'primarch' : 'dorn',
         'traitor' : False,
-        'number' : 7
+        'number' : 7,
+        'battlecry' : 'For Terra'
     },
     'night lords' : {
         'primarch' : 'konrad curze',
         'traitor' : True,
-        'number' : 8
+        'number' : 8,
+        'battlecry' : 'For the Night'
     },
     'white scars' : {
         'primarch' : 'jaghatai khan',
         'traitor' : False,
-        'number' : 5
+        'number' : 5,
+        'battlecry' : 'For Chogoris'
     },
     'iron hands' : {
         'primarch' : 'ferrus manus',
         'traitor' : False,
-        'number' : 10
+        'number' : 10,
+        'battlecry' : 'For Medusa'
     },
     'blood angels' : {
         'primarch' : 'sanguinius',
         'traitor' : False,
-        'number' : 9
+        'number' : 9,
+        'battlecry' : 'For Baal'
     },
     'death guard' : {
         'primarch' : 'mortarion',
         'traitor' : True,
-        'number' : 14
+        'number' : 14,
+        'battlecry' : 'For Nurgle'
     },
     'dark angels' : {
         'primarch' : 'lion el jonson',
         'traitor' : False,
-        'number' : 1
+        'number' : 1,
+        'battlecry' : 'For Caliban'
     },
     'thousand sons' : {
         'primarch' : 'magnus the red',
         'traitor' : True,
-        'number' : 15
+        'number' : 15,
+        'battlecry' : 'For Tzeentch'
     },
     'space wolves' : {
         'primarch' : 'leman russ',
         'traitor' : False,
-        'number' : 6
+        'number' : 6,
+        'battlecry' : 'For Fenris'
     },
     'word bearers' : {
         'primarch' : 'lorgar',
         'traitor' : True,
-        'number' : 17
+        'number' : 17,
+        'battlecry' : 'For Chaos'
     },
     'alpha legion' : {
         'primarch' : 'alfarius',
         'traitor' : True,
-        'number' : 11
+        'number' : 11,
+        'battlecry' : 'For the Hydra'
     },
     'emperor\'s children' : {
         'primarch' : 'fulgrim',
         'traitor' : True,
-        'number' : 4
+        'number' : 4,
+        'battlecry' : 'For Slaanesh'
     },
 }
 
